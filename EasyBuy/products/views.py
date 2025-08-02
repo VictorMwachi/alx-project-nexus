cb298b4956d2434873cbc08c763a2453fbeba079
Victor Mwachi <mudakizevictor@gmail.com>
2025-08-02 23:32:37 +0300

    products app models

--- EasyBuy/products/views.py
+++ EasyBuy/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def product_list(request):
+    # This is a placeholder view function.
+    # You can replace this with actual logic to fetch and display products.
+    dummy_products = [
+        {'name': 'Product 1', 'description': 'Description of Product 1'},
+        {'name': 'Product 2', 'description': 'Description of Product 2'},
+    ]
+    return JsonResponse({'products': dummy_products})
