f6337eecc52480b6bb411883cc1d31383f77ac29
Victor Mwachi <mudakizevictor@gmail.com>
2025-08-04 03:31:02 +0300

    added product image model

--- EasyBuy/users/models.py
+++ EasyBuy/users/models.py
@@ -73,6 +73,7 @@ class Role(models.Model):
 
     def __str__(self):
         return self.name
+    
 class UserRole(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_roles')
